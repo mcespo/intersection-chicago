bb56b0aee5748fd263fb82ad0c5a58dec437f873
mcespo <mcespo@gmail.com>
2021-10-05 01:31:58 -0500

    netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,13 @@
+
+const netlify = require('@sveltejs/adapter-netlify')
 /** @type {import('@sveltejs/kit').Config} */
 import image from "svelte-image";
 
 const config = {
     kit: {
         // hydrate the <div id="svelte"> element in src/app.html
-        target: '#svelte'
+        target: '#svelte',
+        adapter: netlify()
     },
     preprocess: {
         ...image(),
